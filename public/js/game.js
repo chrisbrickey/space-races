35170decabbda4e260ed085c63b180ec3c89d713
chrisbrickey <cbrickey1@gmail.com>
2017-10-29 22:42:59 -0700

    add status of web game and link to github to run command line game

--- public/js/game.js
+++ public/js/game.js
@@ -1,2 +1,16 @@
 const root = document.getElementById("innershell");
-root.innerHTML = "I'm Javascript";
+
+var innerDiv1 = document.createElement('div');
+var innerDiv2 = document.createElement('br');
+var innerDiv3 = document.createElement('div');
+
+innerDiv1.className += "landingText";
+innerDiv2.className += "landingText";
+innerDiv3.className += "landingText";
+
+root.appendChild(innerDiv1);
+root.appendChild(innerDiv2);
+root.appendChild(innerDiv3);
+
+innerDiv1.innerHTML += "This is the landing page for the Space Races web game which is a work-in-progress.";
+innerDiv3.innerHTML += "Play the game from the command line by downloading the files here: https://github.com/chrisbrickey/space-race-game";
